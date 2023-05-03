222fb47218abd92a44edaff33eae97456b61e390
Junaid-ahmed58 <junaid.ahmed@xprolabs.com>
2023-05-03 11:54:21 +0500

    Fixed indentation in user_courses__controller

--- app/controllers/users/courses_controller.rb
+++ app/controllers/users/courses_controller.rb
@@ -1,12 +1,11 @@
 class Users::CoursesController < UserBaseController
   def index
-		enrolled_courses = current_user.enrollments.pluck(:course_id)
-		@courses = Course.where.not(id: enrolled_courses)
-	end
+   enrolled_courses = current_user.enrollments.pluck(:course_id)
+   @courses = Course.where.not(id: enrolled_courses)
+  end
 
   def in_progress
-		enrolled_courses = current_user.enrollments.pluck(:course_id)
-		@courses = Course.where(id: enrolled_courses)
-	end
-
+   enrolled_courses = current_user.enrollments.pluck(:course_id)
+   @courses = Course.where(id: enrolled_courses)
+  end
 end
