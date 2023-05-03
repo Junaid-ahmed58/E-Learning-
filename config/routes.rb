b6c61001d9522535b474657493bc50143031a979
Junaid-ahmed58 <junaid.ahmed@xprolabs.com>
2023-05-03 12:26:27 +0500

    Fixed indentation in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, path: '/courses/users'
   namespace :users do
-    resources :courses do
-      collection do
-				get 'in_progress'
-			end
-	  end
-     resources :enrollments
+   resources :courses do
+    collection do
+		 get 'in_progress'
+		end
+	 end
+   resources :enrollments
 	end
 
-	devise_for :instructors
-	root to: 'courses#index'
-
+  devise_for :instructors
+  root to: 'courses#index'
   resources :courses
   resources :lessons
 end
